a0a895e866265d244427671658ae86028a2f3c42
ThomasChoi <724thomas@gmail.com>
2022-12-20 21:18:14 +0900

    Initial Commit

---
+++ src/main/java/com/example/cookiecafewebsite/CookieCafeWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.cookiecafewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieCafeWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookieCafeWebsiteApplication.class, args);
+    }
+
+}
